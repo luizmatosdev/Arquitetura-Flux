e45b153df5d43e7f249629fff1110227d6165fbd
Luiz Matos <luizmatos88@yahoo.com.br>
2020-07-23 09:22:14 -0300

    Adicionada página de carrinho

--- src/pages/Cart/styles.js
+++ src/pages/Cart/styles.js
@@ -1,3 +1,81 @@
 import styled from 'styled-components'
+import { darken } from 'polished'
 
-export const Container = styled.div``
+export const Container = styled.div`
+  padding: 30px;
+  background-color: #ffffff;
+  border-radius: 4px;
+  footer {
+    margin-top: 30px;
+    display: flex;
+    justify-content: space-between;
+    button {
+      background-color: #5aaeb8;
+      color: #ffffff;
+      border: 0;
+      border-radius: 4px;
+      padding: 12px 20px;
+      font-weight: bold;
+      text-transform: uppercase;
+      transition: background-color 0.2s;
+      &:hover {
+        background-color: ${darken(0.03, '#5aaeb8')};
+      }
+    }
+  }
+`
+
+export const ProductTable = styled.table`
+  width: 100%;
+  thead th {
+    color: #999999;
+    text-align: left;
+    padding: 12px;
+  }
+  tbody td {
+    padding: 12px;
+    border-bottom: 1px solid #eeeeee;
+  }
+  img {
+    height: 100px;
+  }
+  strong {
+    color: #333333;
+    display: block;
+  }
+  span {
+    display: block;
+    margin-top: 5px;
+    font-size: 18px;
+    font-weight: bold;
+  }
+  div {
+    display: flex;
+    align-items: center;
+    input {
+      border: 1px solid #dddddd;
+      border-radius: 4px;
+      color: #666666;
+      padding: 6px;
+      width: 50px;
+    }
+  }
+  button {
+    background: none;
+    border: 0;
+    padding: 6px;
+  }
+`
+
+export const Total = styled.div`
+  display: flex;
+  align-items: baseline;
+  span {
+    color: #999999;
+    font-weight: bold;
+  }
+  strong {
+    font-size: 28px;
+    margin-left: 5px;
+  }
+`
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,122 +1,41 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { MdAddShoppingCart } from 'react-icons/md'
 
+import api from '../../services/api'
+import { formatPrice } from '../../util/format'
 import { ProductList } from './styles'
-export default function Home() {
-  return (
-    <ProductList>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_zoom2.jpg?ts=1586961574&ims=326x"
-          alt="Tênis"
-        />
-        <strong>Tênis muito legal</strong>
-        <span>R$179,90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#ffffff" />
-          </div>
-          <span>Adicionar ao carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_zoom2.jpg?ts=1586961574&ims=326x"
-          alt="Tênis"
-        />
-        <strong>Tênis muito legal</strong>
-        <span>R$179,90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#ffffff" />
-          </div>
-          <span>Adicionar ao carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_zoom2.jpg?ts=1586961574&ims=326x"
-          alt="Tênis"
-        />
-        <strong>Tênis muito legal</strong>
-        <span>R$179,90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#ffffff" />
-          </div>
-          <span>Adicionar ao carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_zoom2.jpg?ts=1586961574&ims=326x"
-          alt="Tênis"
-        />
-        <strong>Tênis muito legal</strong>
-        <span>R$179,90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#ffffff" />
-          </div>
-          <span>Adicionar ao carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_zoom2.jpg?ts=1586961574&ims=326x"
-          alt="Tênis"
-        />
-        <strong>Tênis muito legal</strong>
-        <span>R$179,90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#ffffff" />
-          </div>
-          <span>Adicionar ao carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_zoom2.jpg?ts=1586961574&ims=326x"
-          alt="Tênis"
-        />
-        <strong>Tênis muito legal</strong>
-        <span>R$179,90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#ffffff" />
-          </div>
-          <span>Adicionar ao carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_zoom2.jpg?ts=1586961574&ims=326x"
-          alt="Tênis"
-        />
-        <strong>Tênis muito legal</strong>
-        <span>R$179,90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#ffffff" />
-          </div>
-          <span>Adicionar ao carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_zoom2.jpg?ts=1586961574&ims=326x"
-          alt="Tênis"
-        />
-        <strong>Tênis muito legal</strong>
-        <span>R$179,90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#ffffff" />
-          </div>
-          <span>Adicionar ao carrinho</span>
-        </button>
-      </li>
-    </ProductList>
-  )
+export default class Home extends Component {
+  state = {
+    products: [],
+  }
+
+  async componentDidMount() {
+    const response = await api.get('/products')
+    const data = response.data.map(product => ({
+      ...product,
+      priceFormatted: formatPrice(product.price),
+    }))
+    this.setState({ products: data })
+  }
+  render() {
+    const { products } = this.state
+
+    return (
+      <ProductList>
+        {products.map(product => (
+          <li key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <strong>{product.title}</strong>
+            <span>{product.priceFormatted}</span>
+            <button type="button">
+              <div>
+                <MdAddShoppingCart size={16} color="#ffffff" />
+              </div>
+              <span>Adicionar ao carrinho</span>
+            </button>
+          </li>
+        ))}
+      </ProductList>
+    )
+  }
 }
