b959de7bb2d93947d1dd33db3fbb5b2416bc3936
Luiz Matos <luizmatos88@yahoo.com.br>
2020-07-23 16:48:25 -0300

    Refatorado actions para cada estado do Redux Separado e, bindActionCreators

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 
 import {
@@ -7,8 +8,9 @@ import {
   MdDelete,
 } from 'react-icons/md'
 
+import * as CartActions from '../../store/modules/cart/actions'
 import { Container, ProductTable, Total } from './styles'
-function Cart({ cart, dispatch }) {
+function Cart({ cart, removeFromCart }) {
   return (
     <Container>
       <ProductTable>
@@ -48,9 +50,7 @@ function Cart({ cart, dispatch }) {
               <td>
                 <button
                   type="button"
-                  onClick={() =>
-                    dispatch({ type: 'REMOVE_FROM_CART', id: product.id })
-                  }
+                  onClick={() => removeFromCart(product.id)}
                 >
                   <MdDelete size={20} color="#5aaeb8" />
                 </button>
@@ -72,4 +72,5 @@ function Cart({ cart, dispatch }) {
 const mapStateToProps = state => ({
   cart: state.cart,
 })
-export default connect(mapStateToProps)(Cart)
+const mapDispatchToProps = dispatch => bindActionCreators(CartActions, dispatch)
+export default connect(mapStateToProps, mapDispatchToProps)(Cart)
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import { MdAddShoppingCart } from 'react-icons/md'
 
 import api from '../../services/api'
 import { formatPrice } from '../../util/format'
 import { ProductList } from './styles'
+import * as CartActions from '../../store/modules/cart/actions'
 class Home extends Component {
   state = {
     products: [],
@@ -20,11 +22,8 @@ class Home extends Component {
   }
 
   handleAddProduct = product => {
-    const { dispatch } = this.props
-    dispatch({
-      type: 'ADD_TO_CART',
-      product,
-    })
+    const { addToCart } = this.props
+    addToCart(product)
   }
 
   render() {
@@ -53,4 +52,6 @@ class Home extends Component {
   }
 }
 
-export default connect()(Home)
+const mapDispatchToProps = dispatch => bindActionCreators(CartActions, dispatch)
+
+export default connect(null, mapDispatchToProps)(Home)
